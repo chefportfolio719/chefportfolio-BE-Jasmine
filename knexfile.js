6ed388e0330c86e339abf60df66f19383428cf3b
claridiva2000 <claridiva2000@gmail.com>
2019-07-11 02:56:59 -0500

    GET/POST recipes and GET/POST chefs working. should be enough to allow you guys to work on the home page. will be a few days before delete/put and routers are done.

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,22 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/chefs.db3'
+      filename: './data/chefs.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
         conn.run('PRAGMA foreign_keys = ON', done);
-      }
+      },
     },
     migrations: {
-      directory: './data/migrations'
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
     },
     seeds: {
-      directory: './data/seeds'
-    }
-  }
+      directory: './data/seeds',
+    },
+  },
 };
